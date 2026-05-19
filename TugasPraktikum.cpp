7c840d19ddc4ed4b271f9d0b3a77afb455bdc61d
NekoKaicho <adecta7@gmail.com>
2026-05-19 20:21:11 +0700

    Tambah RekeningSyariah tanpa potongan admin saat potongAdmin() dipanggil

--- TugasPraktikum.cpp
+++ TugasPraktikum.cpp
@@ -5,3 +5,13 @@ class RekeningBank {
 public:
     virtual void potongAdmin() = 0;
 };
+
+class RekeningSyariah : public RekeningBank {
+private:
+    double saldo;
+public:
+    RekeningSyariah(double saldo) : saldo(saldo) {}
+    void potongAdmin() {
+        cout << "Rekening Syariah: tidak ada potongan, saldo tetap Rp " << saldo << endl;
+    }
+};
